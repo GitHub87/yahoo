9e23f40a21b707d8cbd8726790aade507bea4e31
sbhowmik <sbhowmik@host.docker.internal>
2020-03-11 17:16:52 -0400

    Added Gson capability

--- src/main/java/weather/yahoo/incubation/TestBootStrapper.java
+++ src/main/java/weather/yahoo/incubation/TestBootStrapper.java
@@ -2,24 +2,26 @@ package weather.yahoo.incubation;
 
 import java.io.IOException;
 
-import org.apache.spark.sql.Dataset;
-import org.apache.spark.sql.Encoders;
-import org.apache.spark.sql.Row;
-import org.apache.spark.sql.SparkSession;
+import com.google.gson.Gson;
 
+import weather.yahoo.incubation.beans.Location;
 import weather.yahoo.incubation.beans.WeatherBean;
 
 public class TestBootStrapper {
 	
 	public static void main(String[] args) throws IOException{
 		String json = YahooConnectionTest.getYahooWeatherData();
-		SparkSession session = SparkRDDConvertorFromJsonTest.init();
-		Dataset<Row> dataset =  SparkRDDConvertorFromJsonTest.jsonToRddConvertor(session, json);
-		SparkRDDConvertorFromJsonTest.showDataSet(dataset);
+		//SparkSession session = SparkRDDConvertorFromJsonTest.init();
+		//Dataset<Row> dataset =  SparkRDDConvertorFromJsonTest.jsonToRddConvertor(session, json);
+		//SparkRDDConvertorFromJsonTest.showDataSet(dataset);
 		/*Dataset<Row> locDataset = dataset.select("location");*/
-		Dataset<WeatherBean> weatherBeanDataset = dataset.as(Encoders.bean(WeatherBean.class));
-		WeatherBean weatherBean = (WeatherBean)weatherBeanDataset.collect();
-		System.out.println(weatherBean.getLocation().getCity());
+//		Dataset<WeatherBean> weatherBeanDataset = dataset.as(Encoders.bean(WeatherBean.class));
+//		WeatherBean weatherBean = (WeatherBean)weatherBeanDataset.collect();
+//		System.out.println(weatherBean.getLocation().getCity());
+		
+		Gson gson = new Gson();
+		WeatherBean bean = gson.fromJson(json, WeatherBean.class);
+		System.out.println(bean.getLocation().getCity());
 	}
 
 }
